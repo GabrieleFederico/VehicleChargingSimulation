78ce7a7da2744d34a600efe45c20b3a4dd41dbfc
Gabriele Federico <gab.federico@studenti.unina.it>
2023-03-20 23:34:33 +0100

    added jackson and detailed entitied more

--- VehicleChargingSimulationFE/src/Entities/Scenario.java
+++ VehicleChargingSimulationFE/src/Entities/Scenario.java
@@ -1,5 +1,16 @@
 package Entities;
 
+import java.util.HashMap;
+
 public class Scenario {
 
+	private HashMap<String, Station> _stations;
+	
+	public void RunSimulation() {
+		//TODO: call the python script to run the simulation and send it the whole scenario
+	}
+	
+	public void AddStation(Station newStation) {
+		_stations.put(newStation.GetName(), newStation);
+	}
 }
--- VehicleChargingSimulationFE/src/Entities/Station.java
+++ VehicleChargingSimulationFE/src/Entities/Station.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 public class Station {
 	
 	private HashMap<String, Component> _components;
+	private String _name;
 	private int _strategy;
 	private ArrayList<Vehicle> _vehicles = new ArrayList<>();
 	private int _maxChargePower = 300;
@@ -14,4 +15,8 @@ public class Station {
 	private int _maximumChargingVehicles = 3;
 	private ArrayList<Integer> _chargingPowers = new ArrayList<>(_maximumChargingVehicles);
 	private int _time = 0;
+	
+	public String GetName() {
+		return _name;
+	}
 }
