f292951f466427740c3f6c9e5696c339a917936f
Gabriele Federico <gab.federico@studenti.unina.it>
2023-03-20 01:00:51 +0100

    Started creating java swing gui

--- VehicleChargingSimulationFE/src/GUIs/MainWindow.java
+++ VehicleChargingSimulationFE/src/GUIs/MainWindow.java
@@ -1,15 +1,26 @@
 package GUIs;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JButton;
+import java.awt.Choice;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class MainWindow extends JFrame {
 
 	private JPanel contentPane;
+	private ArrayList<VehicleWidget> vehicleWidgets = new ArrayList<>();
 
 	/**
 	 * Launch the application.
@@ -32,11 +43,97 @@ public class MainWindow extends JFrame {
 	 */
 	public MainWindow() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 712, 450);
+		setBounds(100, 100, 710, 487);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-	}
+		
+		JButton addVehicleButton = new JButton("Add Vehicle");
+		
+		JButton importButton = new JButton("Import");
+		
+		JLabel strategyLabel = new JLabel("Strategy");
+		
+		Choice strategyDropPanel = new Choice();
+		strategyDropPanel.add("FCFS");
+		strategyDropPanel.add("EDF");
+		strategyDropPanel.add("RR");
+		
+		JButton exportButton = new JButton("Export");
+		
+		JButton runButton = new JButton("Run");
+		
+		JPanel vehiclesPanel = new JPanel();
+		
+		VehicleWidget vehicleWidget_1 = new VehicleWidget();
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(vehiclesPanel, GroupLayout.PREFERRED_SIZE, 522, GroupLayout.PREFERRED_SIZE)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(25)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(addVehicleButton, GroupLayout.PREFERRED_SIZE, 105, GroupLayout.PREFERRED_SIZE)
+								.addComponent(strategyLabel, GroupLayout.PREFERRED_SIZE, 85, GroupLayout.PREFERRED_SIZE)
+								.addComponent(strategyDropPanel, GroupLayout.PREFERRED_SIZE, 85, GroupLayout.PREFERRED_SIZE)
+								.addComponent(importButton, GroupLayout.PREFERRED_SIZE, 89, GroupLayout.PREFERRED_SIZE)
+								.addComponent(exportButton, GroupLayout.PREFERRED_SIZE, 89, GroupLayout.PREFERRED_SIZE)
+								.addComponent(runButton, GroupLayout.PREFERRED_SIZE, 89, GroupLayout.PREFERRED_SIZE)))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(57)
+							.addComponent(vehicleWidget_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(19)
+							.addComponent(addVehicleButton, GroupLayout.PREFERRED_SIZE, 32, GroupLayout.PREFERRED_SIZE)
+							.addGap(11)
+							.addComponent(strategyLabel)
+							.addGap(6)
+							.addComponent(strategyDropPanel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addGap(25)
+							.addComponent(importButton)
+							.addGap(11)
+							.addComponent(exportButton)
+							.addGap(12)
+							.addComponent(runButton)
+							.addGap(77)
+							.addComponent(vehicleWidget_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(vehiclesPanel, GroupLayout.PREFERRED_SIZE, 418, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		
+		VehicleWidget vehicleWidget = new VehicleWidget();
+		vehiclesPanel.add(vehicleWidget);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		vehiclesPanel.add(scrollPane);
+		contentPane.setLayout(gl_contentPane);
+		vehiclesPanel.add(new VehicleWidget());
+		
+		addVehicleButton.addActionListener(new ActionListener() {
 
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				VehicleWidget newVehicleWidget = new VehicleWidget();
+				vehicleWidgets.add(newVehicleWidget);
+				scrollPane.add(newVehicleWidget);
+				scrollPane.add(new JButton());
+				revalidate();
+				
+			}
+			
+		});
+		
+		
+	}
 }
+
--- VehicleChargingSimulationFE/src/GUIs/VehicleWidget.java
+++ VehicleChargingSimulationFE/src/GUIs/VehicleWidget.java
@@ -1,42 +1,127 @@
 package GUIs;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 
-public class VehicleWidget extends JFrame {
+public class VehicleWidget extends JComponent {
 
 	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					VehicleWidget frame = new VehicleWidget();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	private JTextField nameTextField;
+	private JTextField arrivalTextField;
+	private JTextField departureTextField;
+	private JTextField capacityTextField;
+	private JTextField chargePowerTextField;
+	private JTextField SOCTextField;
+	private JTextField desiredChargeTextField;
 
 	/**
 	 * Create the frame.
 	 */
 	public VehicleWidget() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 388, 172);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		
+		JLabel nameLabel = new JLabel("Name");
+		
+		JLabel arrivalLabel = new JLabel("Arrival");
+		
+		JLabel departureLabel = new JLabel("Departure");
+		
+		nameTextField = new JTextField();
+		nameTextField.setColumns(10);
+		
+		arrivalTextField = new JTextField();
+		arrivalTextField.setColumns(10);
+		
+		departureTextField = new JTextField();
+		departureTextField.setColumns(10);
+		
+		JLabel capacityLabel = new JLabel("Capacity");
+		
+		capacityTextField = new JTextField();
+		capacityTextField.setColumns(10);
+		
+		JLabel chargePowerLabel = new JLabel("Charge Power");
+		
+		chargePowerTextField = new JTextField();
+		chargePowerTextField.setColumns(10);
+		
+		JLabel SOCLabel = new JLabel("SOC");
+		
+		SOCTextField = new JTextField();
+		SOCTextField.setColumns(10);
+		
+		JLabel desiredChargeLabel = new JLabel("DesiredCharge");
+		
+		desiredChargeTextField = new JTextField();
+		desiredChargeTextField.setColumns(10);
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(5)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(nameLabel, GroupLayout.PREFERRED_SIZE, 46, GroupLayout.PREFERRED_SIZE)
+							.addGap(50)
+							.addComponent(arrivalLabel, GroupLayout.PREFERRED_SIZE, 46, GroupLayout.PREFERRED_SIZE)
+							.addGap(10)
+							.addComponent(departureLabel, GroupLayout.PREFERRED_SIZE, 55, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(nameTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addGap(10)
+							.addComponent(arrivalTextField, GroupLayout.PREFERRED_SIZE, 46, GroupLayout.PREFERRED_SIZE)
+							.addGap(10)
+							.addComponent(departureTextField, GroupLayout.PREFERRED_SIZE, 46, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(capacityLabel, GroupLayout.PREFERRED_SIZE, 46, GroupLayout.PREFERRED_SIZE)
+							.addGap(50)
+							.addComponent(chargePowerLabel)
+							.addGap(10)
+							.addComponent(SOCLabel, GroupLayout.PREFERRED_SIZE, 46, GroupLayout.PREFERRED_SIZE)
+							.addGap(29)
+							.addComponent(desiredChargeLabel, GroupLayout.PREFERRED_SIZE, 86, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(capacityTextField, GroupLayout.PREFERRED_SIZE, 55, GroupLayout.PREFERRED_SIZE)
+							.addGap(41)
+							.addComponent(chargePowerTextField, GroupLayout.PREFERRED_SIZE, 39, GroupLayout.PREFERRED_SIZE)
+							.addGap(39)
+							.addComponent(SOCTextField, GroupLayout.PREFERRED_SIZE, 46, GroupLayout.PREFERRED_SIZE)
+							.addGap(29)
+							.addComponent(desiredChargeTextField, GroupLayout.PREFERRED_SIZE, 46, GroupLayout.PREFERRED_SIZE))))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(6)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(nameLabel)
+						.addComponent(arrivalLabel)
+						.addComponent(departureLabel))
+					.addGap(11)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(nameTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(arrivalTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(departureTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(11)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(capacityLabel)
+						.addComponent(chargePowerLabel)
+						.addComponent(SOCLabel)
+						.addComponent(desiredChargeLabel))
+					.addGap(11)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(capacityTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(chargePowerTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(SOCTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(desiredChargeTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+		);
+		contentPane.setLayout(gl_contentPane);
 	}
-
 }
