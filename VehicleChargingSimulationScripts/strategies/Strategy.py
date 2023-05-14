447819025dbce617b94978e1720d7b8fe2fd70f6
Gabriele Federico <gab.federico@studenti.unina.it>
2023-05-14 12:29:36 +0200

    Update Strategy.py

--- VehicleChargingSimulationScripts/strategies/Strategy.py
+++ VehicleChargingSimulationScripts/strategies/Strategy.py
@@ -10,25 +10,6 @@ class Strategy:
         pass
 
     def run(self, station):
-        pass
-
-    @classmethod
-    def parseStrategy(cls, string):
-        if string.strip() == "FCFS":
-            return FCFS()
-        if string.strip() == "EDF":
-            return EDF()
-        if string.strip() == "ALL":
-            return Strategy()
-
-
-class FCFS(Strategy):
-
-    def __init__(self):
-        super().__init__()
-        self.name = "FCFS"
-
-    def run(self, station):
         sortByArrival(station.waitingVehicles)
         for vehicle in station.waitingVehicles:
             if len(station.chargingVehicles) >= station.maximumChargingVehicles or station.time < vehicle.arrival:
@@ -39,6 +20,28 @@ class FCFS(Strategy):
             vehicle.startingChargeTime = station.time
             self.assignPriority(station.chargingVehicles)
         return
+
+    @classmethod
+    def parseStrategy(cls, string):
+        if string.strip() == "FCFS":
+            return FCFS()
+        if string.strip() == "EDF":
+            return EDF()
+        if string.strip() == "ALL":
+            return Strategy()
+
+    def assignPriority(self, chargingVehicles):
+        pass
+
+
+class FCFS(Strategy):
+
+    def __init__(self):
+        super().__init__()
+        self.name = "FCFS"
+
+    def run(self, station):
+        super().run(station)
 
     def assignPriority(self, vehicles):
         i = 1
@@ -50,19 +53,11 @@ class FCFS(Strategy):
 class EDF(Strategy):
 
     def __init__(self):
+        super().__init__()
         self.name = "EDF"
 
     def run(self, station):
-        sortByArrival(station.waitingVehicles)
-        for vehicle in station.waitingVehicles:
-            if len(station.chargingVehicles) >= station.maximumChargingVehicles or station.time < vehicle.arrival:
-                return
-            station.chargingVehicles.append(vehicle)
-            station.waitingVehicles.remove(vehicle)
-            vehicle.charging = True
-            vehicle.startingChargeTime = station.time
-            self.assignPriority(station.chargingVehicles)
-        return
+        super().run(station)
 
     def assignPriority(self, vehicles):
         sortByDeparture(vehicles)
